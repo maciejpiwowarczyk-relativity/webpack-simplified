bdc8bbf6a01dd42703a133c6754d73d4ced5c473
Maciej Piwowarczyk <maciej.piwowarczyk@relativity.com>
2022-12-24 21:57:44 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,1 @@
+const assets = require.context('./assets', false, /\.(png|jpe?g|svg)$/);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+//https://webpack.js.org/api/stats/ --json flag
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name].[contenthash][ext]',
+  },
+  devtool: false,
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
